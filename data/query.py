44b2d2cc520e19652f8bd34bc4b1376ba315831c
Anand Thakker <vestibule@anandthakker.net>
2015-07-10 14:02:12 -0400

    Query landcover data for given set of small patches

--- data/query.py
+++ data/query.py
@@ -9,9 +9,11 @@ from affine import Affine
 import rasterio
 from rasterio.warp import reproject, RESAMPLING, transform
 
+def corners_from_shape(shape):
+    return [(0,0), (0, shape[0]), (shape[1], shape[0]), (shape[1], 0)]
+
 def get_corners(src_crs, affine, shape, dst_crs):
-    corners = [(0,0), (0, shape[0]), (shape[1], shape[0]), (shape[1], 0)]
-    corners = [affine * p for p in corners]
+    corners = [affine * p for p in corners_from_shape(shape)]
     return transform(src_crs, dst_crs,
             [p[0] for p in corners],
             [p[1] for p in corners])
@@ -25,11 +27,13 @@ def project_query(src_crs, affine, shape, dst_crs, dst_affine):
 parser = argparse.ArgumentParser()
 parser.add_argument('data', metavar='data.TIF', type=str, nargs=1)
 parser.add_argument('--query', metavar='query.TIF', type=str, nargs='+', required=True)
+parser.add_argument('--output', '-o')
 
 argv = parser.parse_args()
 
 query = {}
 query_crs = None
+query_pixel = None
 xs = []
 ys = []
 
@@ -37,6 +41,12 @@ with rasterio.open(argv.data[0]) as data:
     data_crs = data.crs
     data_affine = data.affine
 
+    # for each file in the list of 'query' files, pull out the projection
+    # metadata and save the corner coordinates, but transformed into the
+    # *data*'s pixel coordinate space
+    # Reason: we need bounds in the pixel coord space so we can choose a
+    # window for the windowed read (the data is too huge to read the whole
+    # thing).
     for f in argv.query:
         with rasterio.open(f) as src:
             query[f] = {
@@ -51,58 +61,63 @@ with rasterio.open(argv.data[0]) as data:
             xs.extend([c[0] for c in crn])
             ys.extend([c[1] for c in crn])
 
+            pixel = src.affine[0], src.affine[4]
+            if query_pixel:
+                assert(query_pixel == pixel) 
             if query_crs:
                 assert(query_crs['init'] == src.crs['init'])
 
             query_crs = src.crs
+            query_pixel = pixel
 
+    # these are in the data's pixel space
     ul = (min(xs), min(ys))
     lr = (max(xs), max(ys))
     window = ((ul[1], lr[1]), (ul[0], lr[0]))
     orig_cover = data.read(1, window=window)
     data_affine = data_affine * Affine.translation(ul[0], ul[1])
 
-with rasterio.open('test-extraction.tif', mode='w',
-        driver='GTiff',
-        width=orig_cover.shape[1],
-        height=orig_cover.shape[0],
-        count=1,
-        dtype=numpy.uint8,
-        nodata=0,
-        transform=data_affine * Affine.translation(ul[0], ul[1]),
-        crs=data_crs) as dst:
-
-    dst.write_band(1, orig_cover)
 
 # reproject landcover data into query-rectangles' projection
-crn = get_corners(data_crs, data_affine,
-        orig_cover.shape, query_crs)
-
-ypx = abs(max(crn[1]) - min(crn[1])) / orig_cover.shape[0]
-xpx = abs(max(crn[0]) - min(crn[0])) / orig_cover.shape[1]
-dst_transform = (xpx, 0.0, min(crn[0]), 0.0, -ypx, max(crn[1]))
-cover = numpy.empty(orig_cover.shape, dtype=numpy.uint8)
 print('Reprojecting landcover data.')
-print(dst_transform)
+
+# project the corners into the target crs, and use the min/max x and y
+# to determine an appropriately sized array to receive the reprojection
+crn = get_corners(data_crs, data_affine, orig_cover.shape, query_crs)
+newshape = ( (max(crn[1]) - min(crn[1])) / abs(query_pixel[1]),
+             (max(crn[0]) - min(crn[0])) / abs(query_pixel[0]) )
+# pixel (0,0) --> projection minX, maxY
+dst_transform = Affine(query_pixel[0], 0.0, min(crn[0]),
+                    0.0, query_pixel[1], max(crn[1]))
+cover = numpy.empty(newshape, dtype=numpy.uint8)
 
 reproject(orig_cover, cover, src_transform=data_affine, src_crs=data_crs,
         dst_transform=dst_transform,
         dst_crs=query_crs, resampling=RESAMPLING.mode)
 
-with rasterio.open('test-reprojection.tif', mode='w',
-        driver='GTiff',
-        width=cover.shape[1],
-        height=cover.shape[0],
-        count=1,
-        dtype=numpy.uint8,
-        nodata=0,
-        transform=Affine(*dst_transform),
-        crs=query_crs) as dst:
-
-    dst.write_band(1, cover)
-
 # now that the target data is in the same projection as the query patches,
-# we just need to grab the right rectangle for each patch and run whatever
-# stats we want on it.
+# grab the rectangle corresponding to each patch and write it to a file
+for filename in query:
+    patch = query[filename]
+    #band = numpy.zeros(patch['shape'], dtype=rasterio.uint8)
+    box = corners_from_shape(patch['shape'])
+    box = [patch['affine'] * c for c in box]
+    box = [~dst_transform * c for c in box]
+    rows = min([b[1] for b in box]), max([b[1] for b in box])
+    cols = min([b[0] for b in box]), max([b[0] for b in box])
+    band = cover[rows[0]:rows[1], cols[0]:cols[1]]
 
+    filename = os.path.basename(filename).replace('TIF', 'labels.TIF')
+    filename = os.path.join(argv.output, filename)
+    print('Writing ' + filename)
+    with rasterio.open(filename, mode='w',
+            driver='GTiff',
+            width=band.shape[1],
+            height=band.shape[0],
+            count=1,
+            dtype=rasterio.uint8,
+            nodata=0,
+            transform=patch['affine'],
+            crs=query_crs) as dst:
+        dst.write_band(1, band)
 
